eeffad80891e0494ba10856a62263756c43c2a9a
nourbatniji <nourbatniji@gmail.com>
2024-05-23 15:34:53 +0200

    conv

--- ipc_frame.py
+++ ipc_frame.py
@@ -1,5 +1,16 @@
 import os
-import posix
+#import posix
 import random
+import threading
 
+def conv():
+    convValues = []
+    nr = 6
+    for i in range(nr):
+        rnd = random.randint(20, 95)
+        convValues.append(rnd)
+    print(convValues)
 
+conv_thread = threading.Thread(target=conv)
+conv_thread.start()
+conv_thread.join()
