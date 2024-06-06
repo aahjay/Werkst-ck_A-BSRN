445733f7ab9497a076fa05c2cc06a63378aaf933
Mercansirin <168460656+Mercansirin@users.noreply.github.com>
2024-06-06 15:20:16 +0200

    Update Grundgerüst für stat.py

--- stat.py
+++ stat.py
@@ -1,1 +1,12 @@
+import time
 
+def stat(input_func, output_func):
+    values = []
+    while True:
+        value = input_func()
+        if value is not None:
+            values.append(value)
+            mean = sum(values) / len(values)
+            total = sum(values)
+            output_func(mean, total)
+    
