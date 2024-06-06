df818c48ef0670503889540c62cb748f4cc9b350
nourbatniji <nourbatniji@gmail.com>
2024-06-06 15:44:55 +0200

    Kommentare zu log hinzugefügt

--- log.py
+++ log.py
@@ -3,7 +3,5 @@ import threading
 def log():
     # öffnet eine neue log-Datei (Textdokument) bzw. die bereits im Verzeichnis existierende log-Datei
     with open('log.txt', 'a') as file:
-        #if not conv_values:
-         #   break
         # schreibt die Werte aus conv in die Datei 'log.txt'
         file.write(conv_values + ' \n')
