c8f5b3e7da1ba2c13db51e6193a7ce968edffbbb
betuel68 <betuel.alkan@stud.fra-uas.de>
2024-06-06 15:30:09 +0200

    Update report.py

--- report.py
+++ report.py
@@ -1,3 +1,21 @@
 import time
 
-def report
+def report():
+
+    mq_stat_report = posix_ipc.MessageQueue(MQ_STAT_REPORT)
+
+   
+
+    try:
+
+        while True:
+
+            message, _ = mq_stat_report.receive()
+
+            mean, total = struct.unpack('ff', message)
+
+            print(f"Mean: {mean}, Total: {total}")
+
+    except KeyboardInterrupt:
+
+        mq_stat_report.close()
