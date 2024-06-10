2159a74774199cb2e6d930a09a5a47a495799877
nourbatniji <nourbatniji@gmail.com>
2024-06-10 18:18:24 +0200

    Anpassung Variablen

--- stat.py
+++ stat.py
@@ -2,9 +2,9 @@ def stat():
     values = []   # Initialisierung einer leere Liste, um die empfangenen Werte zu speichern
     while True:
         value = ()
-         if value:
-                value = int(value) # Konvertiert den Wert in einen Ganzzahlwert
-                values.append(value) # Fügt den Wert zur Liste der empfangenen Werte hinzu
+         if conv_value:
+                conv_value = int(conv_value) # Konvertiert den Wert in einen Ganzzahlwert
+                values.append(conv_value) # Fügt den Wert zur Liste der empfangenen Werte hinzu
                 mean = sum(values) / len(values)  # Berechnet den Mittelwert der Werte
                 total = sum(values) # Berechnet die Summe der Werte
 
