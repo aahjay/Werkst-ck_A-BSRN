3dfd98036cc3bf215aae0351000be5fbefa7d859
nourbatniji <nourbatniji@gmail.com>
2024-06-11 16:45:09 +0200

    conv client socket Funktion

--- conv.py
+++ conv.py
@@ -1,8 +1,16 @@
 import time
 import random
+import threading
 
 #Diese Funktion erstellt zufällige Zahlen, die anschließend in der Konsole ausgegeben werden
-def conv():
-    conv_values = random.randint(1, 100)
-    print(conv_values)
-    time.sleep(5)
+def convHandleClient(clientSocket):
+    try:
+        while True:
+            conv_values = random.randint(1, 100)
+            #print(str(conv_values))
+            clientSocket.sendall(str(conv_values).encode())
+            print("sent " + str(conv_values))
+            time.sleep(5)
+    finally: 
+        clientSocket.close()
+        
