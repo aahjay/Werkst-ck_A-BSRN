51162a79660d87453701f156e6a20281d1e99562
nourbatniji <nourbatniji@gmail.com>
2024-06-11 13:24:44 +0200

    Funktion für fork und Ausführung der Prozesse (main Skript)

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ scripts = {
 
 # Liste der gestarteten Kindprozesse
 processes = []
+
+def fork_and_exec(script):
+    pid = os.fork()
+    if pid == 0:
+        # Kindprozess: Ersetze den Prozess durch das jeweilige Skript (Übergabeparameter d. Funktion)
+        os.execlp(sys.executable, sys.executable, script)
+    else:
+        # Elternprozess: Rückgabe der PID des Kindprozesses
+        return pid
