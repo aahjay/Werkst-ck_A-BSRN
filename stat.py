b217efbdd4c31bb2a71af560835ed27cdefc07ca
nourbatniji <nourbatniji@gmail.com>
2024-06-15 17:29:01 +0200

    stat CLient Socket erstellt

--- stat.py
+++ stat.py
@@ -1,9 +1,21 @@
+import socket
+import time
+
 def stat():
     values = []   # Initialisierung einer leere Liste, um die empfangenen Werte zu speichern
     while True:
+        conv_values = client_socket.recv(1024).decode()
         value = ()
-         if conv_value:
-                conv_value = int(conv_value) # Konvertiert den Wert in einen Ganzzahlwert
-                values.append(conv_value) # Fügt den Wert zur Liste der empfangenen Werte hinzu
-                mean = sum(values) / len(values)  # Berechnet den Mittelwert der Werte
-                total = sum(values) # Berechnet die Summe der Werte
+        if not conv_values:
+            break
+        conv_value = int(conv_values) # Konvertiert den Wert in einen Ganzzahlwert
+        values.append(conv_value) # Fügt den Wert zur Liste der empfangenen Werte hinzu
+        mean = sum(values) / len(values)  # Berechnet den Mittelwert der Werte
+        total = sum(values) # Berechnet die Summe der Werte
+
+def statClient(mean, total):
+    #Erstellung eiens Clients, der die von Stat verarbeiteten Daten an Report sendet
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 9999))
+    client_socket.sendall(str(mean).encode() + str(total).encode())
+    client_socket.close()
