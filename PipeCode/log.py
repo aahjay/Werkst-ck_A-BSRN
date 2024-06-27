221831a61f1f2c313e027e58ebdb6a4aa5d57f00
Mercansirin <yurtsever.mercan@gmx.de>
2024-06-27 22:23:14 +0200

    Vorläufiger Log Code

---
+++ PipeCode/log.py
@@ -0,0 +1,21 @@
+import os
+
+# Benannte Pipe zum Lesen öffnen
+if not os.path.exists('conv_to_log'):
+    pass
+
+def log():
+    try:
+        with open('conv_to_log', 'r') as pipe_log, open('log.txt', 'a') as logfile:
+            while True:
+                # Messwerte aus der Pipe lesen
+                value = pipe_log.readline().strip()
+                if value:
+                    # Messwert in die Logdatei schreiben
+                    logfile.write(f"{value}\n")
+                    logfile.flush()  # Sicherstellen, dass die Daten sofort geschrieben werden
+    except KeyboardInterrupt:
+        pass
+
+
+
