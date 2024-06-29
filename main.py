c7d88243a599c68a5b4e11d991da980533b455d2
blalkan68 <betuel.alkan@stud.fra-uas.de>
2024-06-29 22:51:12 +0200

    Hinzufügen einer Main

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import signal
+import sys
+import time
+
+# dictionary für die Prozess-Skripte
+scripts = {
+    "conv": "conv.py",
+    "log": "log.py",
+    "stat": "stat_process.py",
+    "report": "report.py"
+}
+
+# Liste der gestarteten Kindprozesse
+processes = []
+
+def signal_handler(sig, frame):
+    # Beenden aller gestarteten Prozesse
+    for pid in processes:
+        try:
+            print('-- terminating processes --')
+            os.kill(pid, signal.SIGTERM)
+        except OSError:
+            pass
+    sys.exit(0)
+
+ # Signal-Handler für SIGINT
+signal.signal(signal.SIGINT, signal_handler)
+    
+def fork_and_exec(script):
+    pid = os.fork()
+    if pid == 0:
+        # Kindprozess: Ersetzen des Prozesses durch das jeweilige Skript (Übergabeparameter d. Funktion)
+        os.execlp(sys.executable, sys.executable, script)
+    else:
+        # Elternprozess: Rückgabe der PID des Kindprozesses
+        return pid
+
+try:
+    # Starten der einzelnen Prozesse
+    for name, script in scripts.items():
+        pid = fork_and_exec(script)
+        time.sleep(2)
+        print (f'Starting {name} process...\n')
+        processes.append(pid)
+        
+    for pid in processes:
+        os.waitpid(pid, 0)
+
+    # Abfangen von Fehlern bei der Ausführung
+except Exception as e:
+    print("-- Fehler beim Starten der Prozesse --")
+    signal_handler(None, None)
